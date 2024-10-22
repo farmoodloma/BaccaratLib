075417067c2acbbdc25f26605f75dac1f938ff6e
farmoodloma <mohd.farmood@lomatechnology.com>
2024-10-22 16:14:42 +0700

    Initial Commit

---
+++ BaccaratLib/BaccaratLib.h
@@ -0,0 +1,18 @@
+//
+//  BaccaratLib.h
+//  BaccaratLib
+//
+//  Created by Mohd Farmood on 22/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BaccaratLib.
+FOUNDATION_EXPORT double BaccaratLibVersionNumber;
+
+//! Project version string for BaccaratLib.
+FOUNDATION_EXPORT const unsigned char BaccaratLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BaccaratLib/PublicHeader.h>
+
+
