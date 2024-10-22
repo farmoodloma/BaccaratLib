7345f416c4ba226313e7f5fb2468b6718e65d09f
farmoodloma <mohd.farmood@lomatechnology.com>
2024-10-22 16:30:15 +0700

    Initial project setup

---
+++ BaccaratLibExample/BaccaratLibExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaccaratLibExampleApp.swift
+//  BaccaratLibExample
+//
+//  Created by Mohd Farmood on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BaccaratLibExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
